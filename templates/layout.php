9ae650aef31d610b0226f59713f4fb798c38507c
euggrush <euggrush@gmail.com>
2021-01-04 14:23:17 -0800

    module3-task1 сделал шаблоны

--- templates/layout.php
+++ templates/layout.php
@@ -1,20 +1,10 @@
-<?php
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-// показывать или нет выполненные задачи
-$show_complete_tasks = rand(0, 1);
-$title = "Дела в порядке";
-?>
 <!DOCTYPE html>
 <html lang="ru">
 
 <head>
     <meta charset="UTF-8">
     <title>
-        <?php
-    echo $title;
-    ?>
+        <?= $title; ?>
     </title>
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/style.css">
@@ -37,7 +27,7 @@ $title = "Дела в порядке";
 
                     <div class="main-header__side-item user-menu">
                         <div class="user-menu__data">
-                            <p>Константин</p>
+                            <p><?= $userName; ?></p>
 
                             <a href="#">Выйти</a>
                         </div>
@@ -45,84 +35,10 @@ $title = "Дела в порядке";
                 </div>
             </header>
 
-            <?php
-                $arrayProjects = [
-                    ['id' => 1,
-                    'name' => "Входящие"],
-                    ['id' => 2,
-                    'name' => "Учеба"],
-                    ['id' => 3,
-                    'name' => "Работа"],
-                    ['id' => 4,
-                    'name' => "Домашние дела"],
-                    ['id' => 5,
-                    'name' => "Авто"]
-                ];
+            <div class="content">
+                <?=require('main.php')?>
 
-                $idNameProjects = array_combine(
-                    array_column($arrayProjects, 'id'),
-                    array_column($arrayProjects, 'name')
-                    );
-
-                $arrayTasks = [
-                    [
-                        'id' => 1,
-                        'name' => 'Собеседование в IT компании',
-                        'date' => '01.12.2019',
-                        'category' => 3,
-                        'isDone' => false
-                    ],
-                    [
-                        'id' => 2,
-                        'name' => 'Выполнить тестовое задание',
-                        'date' => '25.12.2019',
-                        'category' => 3,
-                        'isDone' => false
-                    ],
-                    [
-                        'id' => 3,
-                        'name' => 'Сделать задание первого раздела',
-                        'date' => '21.12.2019',
-                        'category' => 2,
-                        'isDone' => true
-                    ],
-                    [
-                        'id' => 4,
-                        'name' => 'Встреча с другом',
-                        'date' => '22.12.2019',
-                        'category' => 1,
-                        'isDone' => false
-                    ],
-                    [
-                        'id' => 5,
-                        'name' => 'Купить корм для кота',
-                        'date' => null,
-                        'category' => 4,
-                        'isDone' => false
-                    ],
-                    [
-                        'id' => 6,
-                        'name' => 'Заказать пиццу',
-                        'date' => null,
-                        'category' => 4,
-                        'isDone' => false
-                    ],
-                ];
-
-
-
-                function getTasksCount($projectList, $projectName) {
-                    $i = 0;
-                    foreach ($projectList as $project) {
-                        global $idNameProjects;
-                        if ($idNameProjects[$project['category']] === $projectName) {
-                            $i++;
-                        }
-                    }
-                    return $i;
-                }
-                echo $mainContent;
-                ?>
+            </div>
 
         </div>
     </div>
