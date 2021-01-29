ff14d78e5a83679caac3205b61d2702bc2c6d8b1
euggrush <euggrush@gmail.com>
2021-01-28 23:52:13 -0800

    add

--- queries.sql
+++ queries.sql
@@ -12,7 +12,7 @@ INSERT INTO task SET date_create = '07.02.2021',  name = 'Собеседован
 INSERT INTO task SET date_create = '15.01.2021',  name = 'Выполнить тестовое задание', status = 0;
 INSERT INTO task SET date_create = '12.02.2021',  name = 'Сделать задание первого раздела', status = 1;
 INSERT INTO task SET date_create = '14.01.2021',  name = 'Встреча с другом', status = 0;
-INSERT INTO task SET date_create = '',  name = 'Купить корм для кота', status = 0;
+INSERT INTO task SET name = 'Купить корм для кота', status = 0;
 INSERT INTO task SET date_create = '24.01.2021',  name = 'Заказать пиццу', status = 0;
 
 -- ЧТЕНИЕ ЗАПИСЕЙ ИЗ ПРОЕКТОВ
