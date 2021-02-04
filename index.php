88e56389923969fba2972ea6255ad79006dd0e64
euggrush <euggrush@gmail.com>
2021-02-04 10:11:37 -0800

    рефакторинг

--- index.php
+++ index.php
@@ -28,20 +28,10 @@ function getData() {
     return $arrayData;
 }
 
-$arrayProjects = getData()['projects'];
-$arrayTasks = getData()['tasks'];
+$data = getData();
 
-// function getTasksArray() {
-//     $sqlTasks = "SELECT id, project_id, isDone, name, due_date FROM task";
-//     $resultTasks = mysqli_query(connectDb(), $sqlTasks);
-//     return mysqli_fetch_all($resultTasks, MYSQLI_ASSOC);
-// }
-
-// function getProjectsArray() {
-//     $sqlProjects = "SELECT id, name FROM project";
-//     $resultProjects = mysqli_query(connectDb(), $sqlProjects);
-//     return mysqli_fetch_all($resultProjects, MYSQLI_ASSOC);
-// }
+$arrayProjects = $data['projects'];
+$arrayTasks = $data['tasks'];
 
 function getFilterArray(array $arrayToFilter) {
 
